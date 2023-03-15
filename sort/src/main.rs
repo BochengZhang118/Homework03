8647c4f06ae409b4ab7db2da13891d438532bc78
BochengZhang118 <bocheng.zhang19@student.xjtlu.edu.cn>
2023-03-15 17:37:57 +0800

    test11

--- sort/src/main.rs
+++ sort/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    
+ 
+    let mut list = vec![19, 5, 34, 29, 67, 16, 59, 199];
+    println!("Original :{:?}  ", list);
+    bubble_sort(&mut list);
+    println!("After sorting :{:?}  ", list);
+ 
+}
+ 
+
+ 
+fn bubble_sort<T: PartialOrd + Copy>(list: &mut Vec<T>) -> &Vec<T> {
+    for _i in 0..list.len() {
+        for j in 0..list.len() - 1 {
+            if list[j] > list[j + 1] {
+                list.swap(j, j + 1);
+            }
+        }
+    }
+    list
 }
